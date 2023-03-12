d27da0872c97757751201e56439f5637d0902d16
Lê Khắc Thanh Tùng <87592549+ThanhTung2410@users.noreply.github.com>
2023-03-12 13:14:04 +0700

    add: carousel for client page

--- index.php
+++ index.php
@@ -173,6 +173,43 @@
 	</nav>
 
 	<!-- End: Navbar -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-9 mt-4">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="img/banner/vn-50009109-a720785101836d775b0d73c067466489_xxhdpi.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/banner/vn-50009109-a720785101836d775b0d73c067466489_xxhdpi.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/banner/vn-50009109-a720785101836d775b0d73c067466489_xxhdpi.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+			<div class="col-3 mt-4 pl-0">
+				<img class="d-block w-100 h-50 py-1" src="img/banner/vn-50009109-a720785101836d775b0d73c067466489_xxhdpi.jpg" alt="Third slide">
+				<img class="d-block w-100 h-50 pt-1" src="img/banner/vn-50009109-a720785101836d775b0d73c067466489_xxhdpi.jpg" alt="Third slide">
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>

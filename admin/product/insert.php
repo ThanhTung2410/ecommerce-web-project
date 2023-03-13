e535eb794fdebc2764469aa6e0dca1da277956bd
Lê Khắc Thanh Tùng <87592549+ThanhTung2410@users.noreply.github.com>
2023-03-13 22:31:03 +0700

    fix: insert of product

--- admin/product/insert.php
+++ admin/product/insert.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (empty($_POST['name']) &&  empty($_POST['price']) &&  empty($_POST['description']) &&  empty($_POST['manufacturer_id']) && $_FILES['img']['error']) {
+if (empty($_POST['name']) ||  empty($_POST['price']) ||  empty($_POST['description']) ||  empty($_POST['manufacturer_id'])) {
 	$_SESSION['errorMsg'] = "Invalid please submit again with valid information";
 	header("location: form_insert.php");
 	exit;
@@ -11,15 +11,22 @@ $name = $_POST['name'];
 $price = $_POST['price'];
 $description = $_POST['description'];
 $manufacturer_id = $_POST['manufacturer_id'];
+$uploaded_file = $_FILES['img'];
 
 require_once "../handle_single_file.php";
 
-$uploaded_file = $_FILES['img'];
-$max_file_size = 2 * 1024 * 1024;
-$accepted_extensions = array('png', 'jpg');
-$dir = "../../img/product/";
-$locationToRedirect = "form_insert.php";
-$path = handle_uploaded_file($uploaded_file, $max_file_size, $accepted_extensions, $dir, $locationToRedirect);
+$path = null;
+if (!$uploaded_file['error']) {
+	$max_file_size = 2 * 1024 * 1024;
+	$accepted_extensions = array('png', 'jpg');
+	$dir = "../../img/product/";
+	$locationToRedirect = "form_insert.php";
+	$path = handle_uploaded_file($uploaded_file, $max_file_size, $accepted_extensions, $dir, $locationToRedirect);
+} else {
+	$_SESSION['errorMsg'] = "Can not upload image";
+	header("location: form_insert.php");
+	exit;
+}
 
 require_once "../../connect.php";
 $sql = "INSERT INTO product(name, price, description, imgPath, manufacturer_id) VALUES(?, ?, ?, ?, ?)";
